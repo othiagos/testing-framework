4a6f7d73d00201eb3bbd7d0a793aa9e83f10f884
othiagos <thg.santos@outlook.com>
2026-04-10 18:35:30 -0300

    Classes TestLoader e TestRunner

--- tests/test.py
+++ tests/test.py
@@ -1,106 +1,46 @@
 from src.test_case import TestCase
-from src.test_result import TestResult
 from src.test_stub import TestStub
 from src.test_spy import TestSpy
 from src.test_suite import TestSuite
+from src.test_loader import TestLoader
+from src.test_runner import TestRunner
 
-class TestCaseTest(TestCase):
+class TestLoaderTest(TestCase):
 
-    def set_up(self):
-        self.result = TestResult()
-
-    def test_result_success_run(self):
-        stub = TestStub('test_success')
-        stub.run(self.result)
-        assert self.result.summary() == '1 run, 0 failed, 0 error'
-
-    def test_result_failure_run(self):
-        stub = TestStub('test_failure')
-        stub.run(self.result)
-        assert self.result.summary() == '1 run, 1 failed, 0 error'
-
-    def test_result_error_run(self):
-        stub = TestStub('test_error')
-        stub.run(self.result)
-        assert self.result.summary() == '1 run, 0 failed, 1 error'
-
-    def test_result_multiple_run(self):
-        stub = TestStub('test_success')
-        stub.run(self.result)
-        stub = TestStub('test_failure')
-        stub.run(self.result)
-        stub = TestStub('test_error')
-        stub.run(self.result)
-        assert self.result.summary() == '3 run, 1 failed, 1 error'
-
-    def test_was_set_up(self):
-        spy = TestSpy('test_method')
-        spy.run(self.result)
-        assert spy.was_set_up
-
-    def test_was_run(self):
-        spy = TestSpy('test_method')
-        spy.run(self.result)
-        assert spy.was_run
-
-    def test_was_tear_down(self):
-        spy = TestSpy('test_method')
-        spy.run(self.result)
-        assert spy.was_tear_down
-
-    def test_template_method(self):
-        spy = TestSpy('test_method')
-        spy.run(self.result)
-        assert spy.log == "set_up test_method tear_down"
-
-
-class TestSuiteTest(TestCase):
-
-    def test_suite_size(self):
-        suite = TestSuite()
-
-        suite.add_test(TestStub('test_success'))
-        suite.add_test(TestStub('test_failure'))
-        suite.add_test(TestStub('test_error'))
-
+    def test_create_suite(self):
+        loader = TestLoader()
+        suite = loader.make_suite(TestStub)
         assert len(suite.tests) == 3
 
-    def test_suite_success_run(self):
-        result = TestResult()
+    def test_create_suite_of_suites(self):
+        loader = TestLoader()
+        stub_suite = loader.make_suite(TestStub)
+        spy_suite = loader.make_suite(TestSpy)
+
         suite = TestSuite()
-        suite.add_test(TestStub('test_success'))
+        suite.add_test(stub_suite)
+        suite.add_test(spy_suite)
 
-        suite.run(result)
+        assert len(suite.tests) == 2
 
-        assert result.summary() == '1 run, 0 failed, 0 error'
+    def test_get_multiple_test_case_names(self):
+        loader = TestLoader()
+        names = loader.get_test_case_names(TestStub)
+        assert names == ['test_error', 'test_failure', 'test_success']
 
-    def test_suite_multiple_run(self):
-        result = TestResult()
-        suite = TestSuite()
-        suite.add_test(TestStub('test_success'))
-        suite.add_test(TestStub('test_failure'))
-        suite.add_test(TestStub('test_error'))
+    def test_get_no_test_case_names(self):
 
-        suite.run(result)
+        class Test(TestCase):
+            def foobar(self):
+                pass
 
-        assert result.summary() == '3 run, 1 failed, 1 error'
+        loader = TestLoader()
+        names = loader.get_test_case_names(Test)
+        assert names == []
 
 
-result = TestResult()
-suite = TestSuite()
+loader = TestLoader()
+suite = loader.make_suite(TestLoaderTest)
 
-suite.add_test(TestCaseTest('test_result_success_run'))
-suite.add_test(TestCaseTest('test_result_failure_run'))
-suite.add_test(TestCaseTest('test_result_error_run'))
-suite.add_test(TestCaseTest('test_result_multiple_run'))
-suite.add_test(TestCaseTest('test_was_set_up'))
-suite.add_test(TestCaseTest('test_was_run'))
-suite.add_test(TestCaseTest('test_was_tear_down'))
-suite.add_test(TestCaseTest('test_template_method'))
-
-suite.add_test(TestSuiteTest('test_suite_size'))
-suite.add_test(TestSuiteTest('test_suite_success_run'))
-suite.add_test(TestSuiteTest('test_suite_multiple_run'))
-
-suite.run(result)
-print(result.summary())
+runner = TestRunner()
+runner.run(suite)
